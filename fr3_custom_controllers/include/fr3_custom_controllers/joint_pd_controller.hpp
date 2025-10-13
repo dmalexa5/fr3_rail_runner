5f0d1ccc75606e9110219bab2e77d7dc727ef20f
David Alexander <dmalexa5@ncsu.edu>
2025-10-13 19:49:30 -0400

    Working joint_pd_controller with hardcoded params

--- fr3_custom_controllers/include/fr3_custom_controllers/joint_pd_controller.hpp
+++ fr3_custom_controllers/include/fr3_custom_controllers/joint_pd_controller.hpp
@@ -9,7 +9,7 @@
 #include <unordered_map>
 #include <utility>
 #include <vector>
-#include <eigen3/Eigen/Dense>
+
 #include <rclcpp/duration.hpp>
 #include <rclcpp/time.hpp>
 
@@ -18,11 +18,6 @@
 using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
 
 namespace fr3_custom_controllers {
-  
-  struct JointGains {
-    double p;
-    double d;
-  };
 
   class JointPDController : public controller_interface::ControllerInterface 
   {
@@ -30,11 +25,9 @@ namespace fr3_custom_controllers {
     public:
       JointPDController();
 
-      using Vector8d = Eigen::Matrix<double, 8, 1>;
+      [[nodiscard]] controller_interface::InterfaceConfiguration command_interface_configuration() const override;
 
-      controller_interface::InterfaceConfiguration command_interface_configuration() const override;
-
-      controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+      [[nodiscard]] controller_interface::InterfaceConfiguration state_interface_configuration() const override;
 
       controller_interface::return_type update(
         const rclcpp::Time & time, const rclcpp::Duration & period) override;
@@ -46,21 +39,10 @@ namespace fr3_custom_controllers {
 
       controller_interface::CallbackReturn on_activate(
         const rclcpp_lifecycle::State & previous_state) override;
-      
-      controller_interface::CallbackReturn on_deactivate(
-        const rclcpp_lifecycle::State & previous_state) override;
+
 
     private:
       
-      double q; // current joint position
-      double qd; // current joint velocity
-
-      double tau; //output torque
-
-
-
-      std::unordered_map<std::string, JointGains> joint_gains_;
-
       std::vector<std::string> joint_names_;
       std::vector<std::string> command_interface_types_;
       std::vector<std::string> state_interface_types_;
@@ -72,16 +54,9 @@ namespace fr3_custom_controllers {
       std::vector<std::reference_wrapper<hardware_interface::LoanedStateInterface>>
         joint_velocity_state_interface_;
 
-      std::unordered_map<
-        std::string, std::vector<std::reference_wrapper<hardware_interface::LoanedCommandInterface>> *>
-        command_interface_map_ = {
-          {"effort", &joint_effort_command_interface_}};
-
-      std::unordered_map<
-        std::string, std::vector<std::reference_wrapper<hardware_interface::LoanedStateInterface>> *>
-        state_interface_map_ = {
-          {"position", &joint_position_state_interface_},
-          {"velocity", &joint_velocity_state_interface_}};
+      double q;
+      double qd;
+      double tau;
       };
 
 }
